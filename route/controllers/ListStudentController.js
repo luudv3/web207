00f032bfda3a966315ed0c83630485a4799d6486
luubang201 <dangvuluu201@gmail.com>
2023-11-28 15:19:15 +0700

    update

---
+++ route/controllers/ListStudentController.js
@@ -0,0 +1,21 @@
+window.ListStudentController = function($scope, $http){
+    $scope.title = "Danh sách sinh viên";
+
+    // Thực hiện call API
+    // Link API
+
+    const apiStudents= "http://localhost:3000/students";
+    function getData(){
+        $http.get(apiStudents).then(function(response){
+            console.log(response);
+
+        // truy cập dữ liệu
+            console.log(response.data);
+
+            if(response.status == 200){
+                $scope.listStudents = response.data;
+            }
+        })
+    }
+    getData();
+}
